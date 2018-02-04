9a978f456dbac9d5af11d22ae2153e6497ecb1f1
BlastingJavier <31637787+BlastingJavier@users.noreply.github.com>
2018-02-05 00:36:41 +0100

    Update types.h

--- types.h
+++ types.h
@@ -2,9 +2,9 @@
  * @brief It defines common types
  *
  * @file types.h
- * @author Profesores PPROG
+ *@author FJNR & AMH
  * @version 1.0
- * @date 13-01-2015
+ *@date 05/02/2018
  * @copyright GNU Public License
  */
 
@@ -15,15 +15,21 @@
 #define NO_ID -1
 
 typedef long Id;
-
+/**
+Enumeración de códigos de error DE BOOL
+*/
 typedef enum {
   FALSE, TRUE
 } BOOL;
-
+/**
+Enumeración de códigos de error de STATUS
+*/
 typedef enum {
   ERROR, OK
 } STATUS;
-
+/**
+Enumeración de direcciones
+*/
 typedef enum {
   N, S, E, W
 } DIRECTION;
