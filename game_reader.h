f22f56175921c334409e6b411838843c94097c25
BlastingJavier <31637787+BlastingJavier@users.noreply.github.com>
2018-02-06 00:49:24 +0100

    Add files via upload

---
+++ game_reader.h
@@ -0,0 +1,17 @@
+/*
+*@brief Carga de espacios (interfaz)
+*
+*
+*@file game_reader.h
+*@author FJNR & AMH
+*@version 1.0
+*@date 05/02/2018
+*@copyright GNU Public License
+*/
+#ifndef GAME_READ
+#define GAME_READ
+#include "game.h"
+
+STATUS game_load_spaces(Game* game, char* filename);
+
+#endif
