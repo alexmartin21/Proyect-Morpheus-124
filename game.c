d0a9631842b7560b5439a66127f3a93cd1c3ed00
BlastingJavier <31637787+BlastingJavier@users.noreply.github.com>
2018-02-09 02:45:43 +0100

    Update game.c

--- game.c
+++ game.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "game.h"
+#include "game_reader.h"
 
 #define N_CALLBACK 4
 
@@ -48,7 +49,6 @@ void game_callback_previous(Game* game);
 /**
 Array de punteros a funciones
 static = unico en este modulo (evitar que se exponga) (cuidadin)
-
 */
 static callback_fn game_callback_fn_list[N_CALLBACK]={
   game_callback_unknown,
@@ -59,10 +59,6 @@ static callback_fn game_callback_fn_list[N_CALLBACK]={
 /**
    Funciones privadas, las cuales solo se ponen en el fichero.c
 */
-
-STATUS game_load_spaces(Game* game, char* filename);
-STATUS game_add_space(Game* game, Space* space);
-/*P. F. */
 Id     game_get_space_id_at(Game* game, int position);
 STATUS game_set_player_location(Game* game, Id id);
 STATUS game_set_object_location(Game* game, Id id);
@@ -97,7 +93,7 @@ STATUS game_create_from_file(Game* game, char* filename) {
   if (game_create(game) == ERROR)
     return ERROR;
 
-  if (game_load_spaces(game, filename) == ERROR)
+  if (game_reader_load_spaces(game, filename) == ERROR)
     return ERROR;
 
   game_set_player_location(game, game_get_space_id_at(game, 0));
@@ -118,10 +114,12 @@ STATUS game_destroy(Game* game) {
 
   return OK;
 }
-/**
-   TAD (ESTADO): completa el array de space un espacio desde que esta vacio
-   -Si los argumentos estan vacios = ERROR
-*/
+/*
+ * @brief funcionalidad de añadir espacios
+ * @param game, puntero a estructura Game (direccion)
+ * @param space , puntero a estructura Space (direccion)
+ * @return status, OK O ERROR
+ */
 STATUS game_add_space(Game* game, Space* space) {
   int i = 0;
 
@@ -141,11 +139,12 @@ STATUS game_add_space(Game* game, Space* space) {
 
   return OK;
 }
-/**kajldfkjal;kdjf;lakjdfkajdlkfja;ldjf;lakjdflkjad;lfkja;ldkjfla;kjdf
-   TAD (ID): completa el array de space un espacio desde que esta vacio
-   -Si los argumentos estan vacios = ERROR
-*/
-
+/*
+ * @brief funcionalidad leer el id de un espacio
+ * @param game, puntero a estructura,(direccion)
+ * @param position, posicion del espacio (en el array de punteros a Space)
+ * @return NO_ID (si la posicion se sale de limites), space_get_id (la posicion)
+ */
 Id game_get_space_id_at(Game* game, int position) {
 
   if (position < 0 || position >= MAX_SPACES) {
@@ -154,11 +153,6 @@ Id game_get_space_id_at(Game* game, int position) {
 
   return space_get_id(game->spaces[position]);
 }
-/**
-   TAD (GET): completa el array de space un espacio desde que esta vacio
-   -Si los argumentos estan vacios = ERROR
-*/
-
 Space* game_get_space(Game* game, Id id){
   int i = 0;
 
@@ -174,11 +168,12 @@ Space* game_get_space(Game* game, Id id){
 
   return NULL;
 }
-
-/**
-   SET PLAYER LOCATION: Comprueba el error comparando la macro y establece la posicion del jugador en el juego
-*/
-/*P. F. */
+/*
+ * @brief funcionalidad de modificar la localizacion del jugador
+ * @param game, puntero a Game (direccion)
+ * @param id, que nos sirve para asginar el id de la nueva localizacion
+ * @return ERROR, en el caso de que lo hubiera
+ */
 STATUS game_set_player_location(Game* game, Id id) {
 
   if (id == NO_ID) {
@@ -188,11 +183,12 @@ STATUS game_set_player_location(Game* game, Id id) {
   game->player_location = id;
 
 }
-
-/**
-   SET OBJECT LOCATION: Comprueba el error comprarando la macro y establece la posicion del objeto en el juego
-*/
-/*P. F. */
+/*
+ * @brief funcionalidad de cambio de objetos de localizacion
+ * @param game,puntero a la estructura Game
+ * @param id, campo de la estructura Id
+ * @return status, OK O ERROR
+ */
 STATUS game_set_object_location(Game* game, Id id) {
 
   int i = 0;
@@ -304,7 +300,7 @@ void game_callback_following(Game* game) {
     if (current_id == space_id) {
       current_id = space_get_south(game->spaces[i]);
       if (current_id != NO_ID) {
-	game_set_player_location(game, current_id);
+	       game_set_player_location(game, current_id);
       }
       return;
     }
@@ -327,65 +323,9 @@ void game_callback_previous(Game* game) {
     if (current_id == space_id) {
       current_id = space_get_north(game->spaces[i]);
       if (current_id != NO_ID) {
-	game_set_player_location(game, current_id);
+	       game_set_player_location(game, current_id);
       }
       return;
     }
   }
-}
-/*P. F. */
-STATUS game_load_spaces(Game* game, char* filename) {
-  FILE* file = NULL;
-  char line[WORD_SIZE] = "";
-  char name[WORD_SIZE] = "";
-  char* toks = NULL;
-  Id id = NO_ID, north = NO_ID, east = NO_ID, south = NO_ID, west = NO_ID;
-  Space* space = NULL;
-  STATUS status = OK;
-
-  if (!filename) {
-    return ERROR;
-  }
-
-  file = fopen(filename, "r");
-  if (file == NULL) {
-    return ERROR;
-  }
-
-  while (fgets(line, WORD_SIZE, file)) {
-    if (strncmp("#s:", line, 3) == 0) {
-      toks = strtok(line + 3, "|");
-      id = atol(toks);
-      toks = strtok(NULL, "|");
-      strcpy(name, toks);
-      toks = strtok(NULL, "|");
-      north = atol(toks);
-      toks = strtok(NULL, "|");
-      east = atol(toks);
-      toks = strtok(NULL, "|");
-      south = atol(toks);
-      toks = strtok(NULL, "|");
-      west = atol(toks);
-#ifdef DEBUG
-      printf("Leido: %ld|%s|%ld|%ld|%ld|%ld\n", id, name, north, east, south, west);
-#endif
-      space = space_create(id);
-      if (space != NULL) {
-	space_set_name(space, name);
-	space_set_north(space, north);
-	space_set_east(space, east);
-	space_set_south(space, south);
-	space_set_west(space, west);
-	game_add_space(game, space);
-      }
-    }
-  }
-
-  if (ferror(file)) {
-    status = ERROR;
-  }
-
-  fclose(file);
-
-  return status;
 }
