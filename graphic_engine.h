c19f41ff1c8864118cfee432e54e5fbf7ebc0673
alexmartin21 <35938543+alexmartin21@users.noreply.github.com>
2018-02-12 01:09:08 +0100

    Update graphic_engine.h

--- graphic_engine.h
+++ graphic_engine.h
@@ -1,10 +1,10 @@
-/** 
- * @brief It defines a textual graphic engine
- * 
+/**
+ * @brief Define un motor gráfico
+ *
  * @file graphic_engine.h
- * @author Profesores PPROG
- * @version 1.0 
- * @date 18-01-2017
+ *@author FJNR & AMH
+ * @version 1.0
+ *@date 05/02/2018
  * @copyright GNU Public License
  */
 
@@ -13,10 +13,31 @@
 
 #include "game.h"
 
+/*Estructura que define el Graphic_engine tamaño del area*/
 typedef struct _Graphic_engine Graphic_engine;
 
+/*
+ * @brief Tiene la función de crear el área (se generan unos puntos en la
+    pantalla con: (x,y,width,height)) de los interfaces del juego
+    (inicializa la estructura de Graphic_engine)
+ * @param nada
+ * @return "ge", el puntero que contiene los elementos de la estructura
+    (punteros de tipo Area)
+ */
 Graphic_engine* graphic_engine_create();
+/*
+ * @brief Tiene la función de liberar la memoria de todos los campos de ge
+ * @param "ge", el puntero a "Graphic_engine"
+ * @return, ya que es una función de tipo void
+ */
 void graphic_engine_destroy(Graphic_engine *ge);
+/*
+ * @brief Dibuja cada área en la pantalla de salida (se generan puntos en la
+    pantalla con: (x,y,width,height))
+ * @param "ge",  el puntero a "Graphic_engine"
+ * @param "game", el puntero a "Game"
+ * @return, ya que es una función de tipo void
+ */
 void graphic_engine_paint_game(Graphic_engine *ge, Game *game);
 void graphic_engine_write_command(Graphic_engine *ge, char *str);
 
